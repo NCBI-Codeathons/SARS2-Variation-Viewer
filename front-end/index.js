9ef4b865bd1527067cd8ec97832e64125bfa6dc8
Vichet Hem <vichethem@gmail.com>
2020-07-08 11:17:45 -0400

    add front-end dir

---
+++ front-end/index.js
@@ -0,0 +1,5 @@
+import {Cartoon} from "./modules/cartoon.js";
+
+document.addEventListener('DOMContentLoaded', event => {
+  const cartoon = new Cartoon();
+});
---
+++ front-end/modules/cartoon.js
@@ -0,0 +1,6 @@
+export class Cartoon {
+  constructor() {
+    const cartoon = d3.select('#cartoon');
+    cartoon.text('Cartoon goes here...');
+  }
+}
