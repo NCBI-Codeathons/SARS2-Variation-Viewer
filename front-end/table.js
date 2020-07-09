83af976bddc48699cd3c8bc76fa51d30be07df9f
Vichet Hem <vichethem@gmail.com>
2020-07-09 17:04:28 -0400

    render alleles attributes

--- front-end/table.js
+++ front-end/table.js
@@ -4,13 +4,26 @@ export class Table {
       pageLength: 50,
       data: data.variants,
       "columns": [
+        {"data": 'reference_sequence'},
         {"data": 'start'},
         {"data": 'stop'},
-        {"data": 'reference_sequence'},
+
         {
           "data": 'alleles', render: (data, type, row, meta) => {
-            const {allele, count, spdi} = data[0];
-            return `${allele}, count:${count}, spdi:${spdi}`
+            let html = ``;
+            const {allele, count, spdi, attributes} = data[0];
+            html += `${allele}, count:${count}, spdi:${spdi}`
+            if (attributes) {
+              for (const d of attributes) {
+                const {name, values} = d;
+                html += `<br /><b>${name}</b>`
+                for (const dd of values) {
+                  html += `<br />${dd.value} (${dd.count})`
+                }
+
+              }
+            }
+            return html;
           }
         },
       ]
