ab9a79e6cd917ba29fc3f2baf905b39942d38c91
Vichet Hem <vichethem@gmail.com>
2020-07-10 14:06:35 -0400

    done

--- docs/modules/details.js
+++ docs/modules/details.js
@@ -1,30 +1,52 @@
+export const mapArray = [];
+// refseq acc, pdb chain, offset to get prd resi, start, end of pdb resi
+// no data for: YP_009725298, YP_009725300, YP_009725302, YP_009724394, YP_009725318, YP_009724396, YP_009725255, YP_009724393
+mapArray.push('YP_009725297 2GDT_A -11 2 116');
+mapArray.push('YP_009725299 6WUU_A -743 1 318');
+mapArray.push('YP_009725301 6XA4_A 2 4 307');
+mapArray.push('YP_009725303 7BV1_C 1 3 65');
+mapArray.push('YP_009725304 7BV1_B 1 79 192');
+mapArray.push('YP_009725305 6W9Q_A 20 21 133');
+mapArray.push('YP_009725306 6W4H_B 3 21 136');
+mapArray.push('YP_009725307 7C2K_A 2 3 931');
+mapArray.push('YP_009725308 6JYT_A 2 3 598');
+mapArray.push('YP_009725309 5C8S_B 0 1 525');
+mapArray.push('YP_009725310 6XDH_A 22 23 367');
+mapArray.push('YP_009725311 6W4H_A 3 4 301');
+mapArray.push('YP_009724391 6XDC_A 0 40 238');
+mapArray.push('YP_009724395 1XAK_A -13 2 69');
+mapArray.push('YP_009724390 7C2L_A 0 14 1146');
+mapArray.push('YP_009724392 5X29_A 16 24 81');
+mapArray.push('YP_009724397 6YI3_A -40 3 140');
+
+export function getPdbInfo(mapArray, acc_ver, resi) {
+  var pdbinfo = {};
+  var pos = acc_ver.indexOf('.');
+  var acc = (pos == -1) ? acc_ver : acc_ver.substr(0, pos);
+
+  for (var i = 0, il = mapArray.length; i < il; ++i) {
+    var idArray = mapArray[i].split(' ');
+
+    if (idArray.length != 5) console.log("Error in the mapping data");
+
+    if (acc == idArray[0]) {
+      var pdbid = idArray[1];
+      var offset = parseInt(idArray[2]);
+      var pdbResi = resi + offset;
+      if (pdbResi >= parseInt(idArray[3]) && pdbResi <= parseInt(idArray[4])) {
+        pdbinfo.pdbid = pdbid;
+        pdbinfo.resi = pdbResi;
+      }
+
+      break;
+    }
+  }
+
+  return pdbinfo;
+}
+
 export const detailsView = data => {
 
-  function getPdbInfo(mapArray, acc_ver, resi) {
-    var pdbinfo = {};
-    var pos = acc_ver.indexOf('.');
-    var acc = (pos == -1) ? acc_ver : acc_ver.substr(0, pos);
-
-    for (var i = 0, il = mapArray.length; i < il; ++i) {
-      var idArray = mapArray[i].split(' ');
-
-      if (idArray.length != 5) console.log("Error in the mapping data");
-
-      if (acc == idArray[0]) {
-        var pdbid = idArray[1];
-        var offset = parseInt(idArray[2]);
-        var pdbResi = resi + offset;
-        if (pdbResi >= parseInt(idArray[3]) && pdbResi <= parseInt(idArray[4])) {
-          pdbinfo.pdbid = pdbid;
-          pdbinfo.resi = pdbResi;
-        }
-
-        break;
-      }
-    }
-
-    return pdbinfo;
-  }
 
   function getSnpHtml(posHash, range0, range1, bHistogram) {
     var snpHtml = '';
@@ -101,26 +123,6 @@ export const detailsView = data => {
     return 'rgb(255, ' + parseInt(255 - value * 255) + ', ' + parseInt(255 - value * 255) + ')';
   }
 
-  var mapArray = [];
-  // refseq acc, pdb chain, offset to get prd resi, start, end of pdb resi
-  // no data for: YP_009725298, YP_009725300, YP_009725302, YP_009724394, YP_009725318, YP_009724396, YP_009725255, YP_009724393
-  mapArray.push('YP_009725297 2GDT_A -11 2 116');
-  mapArray.push('YP_009725299 6WUU_A -743 1 318');
-  mapArray.push('YP_009725301 6XA4_A 2 4 307');
-  mapArray.push('YP_009725303 7BV1_C 1 3 65');
-  mapArray.push('YP_009725304 7BV1_B 1 79 192');
-  mapArray.push('YP_009725305 6W9Q_A 20 21 133');
-  mapArray.push('YP_009725306 6W4H_B 3 21 136');
-  mapArray.push('YP_009725307 7C2K_A 2 3 931');
-  mapArray.push('YP_009725308 6JYT_A 2 3 598');
-  mapArray.push('YP_009725309 5C8S_B 0 1 525');
-  mapArray.push('YP_009725310 6XDH_A 22 23 367');
-  mapArray.push('YP_009725311 6W4H_A 3 4 301');
-  mapArray.push('YP_009724391 6XDC_A 0 40 238');
-  mapArray.push('YP_009724395 1XAK_A -13 2 69');
-  mapArray.push('YP_009724390 7C2L_A 0 14 1146');
-  mapArray.push('YP_009724392 5X29_A 16 24 81');
-  mapArray.push('YP_009724397 6YI3_A -40 3 140');
 
   var snpData = data.variants;
   var range0 = data.segments[0].range[0], range1 = data.segments[0].range[1];
