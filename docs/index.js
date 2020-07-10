b638e998d2679cbe5a3dc2396d77a5e70ecb9f2b
Brad Holmes <holmesbr@ncbi.nlm.nih.gov>
2020-07-10 14:13:15 -0400

    Fix name of file

--- docs/index.js
+++ docs/index.js
@@ -1,11 +1,11 @@
-import {data} from "./mock-data/protein_annotated_without_metadata.js";
-import {Cartoon} from "./modules/cartoon.js";
-import {Table} from "./modules/table.js";
-import {detailsView} from './modules/details.js';
+import { data } from "./mock-data/protein_and_metadata.js";
+import { Cartoon } from "./modules/cartoon.js";
+import { Table } from "./modules/table.js";
+import { detailsView } from './modules/details.js';
 
 $(document).ready(function () {
-  const cartoon = new Cartoon({data});
-  const table = new Table({data})
+  const cartoon = new Cartoon({ data });
+  const table = new Table({ data })
   $('#details-tab').on('click', e => {
     const $this = $(this);
     if (!$this.data('load')) {
